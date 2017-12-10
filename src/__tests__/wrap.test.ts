95268b1cb6106dbe07b3c06aa6faa704f8c75fdf
Konstantin Gorodinskiy <mail@konstantin.io>
2017-12-10 23:16:50 +0100

    redo in template literals

--- src/__tests__/wrap.test.ts
+++ src/__tests__/wrap.test.ts
@@ -13,53 +13,37 @@ describe("wrap", () => {
 			assert.equal(wrappedText, "--test--");
 		});
 
-		describe("should wrap with correct opening character", () => {
+		describe("should wrap with correct predefined pattern", () => {
 			it("should wrap with {}", () => {
-				const wrappedText = wrap("test", "}");
-				assert.equal(wrappedText, "{test}");
+				assert.equal(wrap("test", "}"), "{test}");
+				assert.equal(wrap("test", "{"), "{test}");
 			});
 
 			it("should wrap with «»", () => {
-				const wrappedText = wrap("test", "»");
-				assert.equal(wrappedText, "«test»");
+				assert.equal(wrap("test", "»"), "«test»");
+				assert.equal(wrap("test", "«"), "«test»");
+			});
+
+			it("should wrap with ()", () => {
+				assert.equal(wrap("test", ")"), "(test)");
+				assert.equal(wrap("test", "("), "(test)");
 			});
 
 			it("should wrap with []", () => {
-				const wrappedText = wrap("test", "]");
-				assert.equal(wrappedText, "[test]");
+				assert.equal(wrap("test", "]"), "[test]");
+				assert.equal(wrap("test", "["), "[test]");
 			});
 
 			it("should wrap with <>", () => {
-				const wrappedText = wrap("test", ">");
-				assert.equal(wrappedText, "<test>");
-			});
-		});
-
-		describe("should wrap with correct closing character", () => {
-			it("should wrap with {}", () => {
-				const wrappedText = wrap("test", "{");
-				assert.equal(wrappedText, "{test}");
-			});
-
-			it("should wrap with «»", () => {
-				const wrappedText = wrap("test", "«");
-				assert.equal(wrappedText, "«test»");
-			});
-
-			it("should wrap with []", () => {
-				const wrappedText = wrap("test", "[");
-				assert.equal(wrappedText, "[test]");
-			});
-
-			it("should wrap with <>", () => {
-				const wrappedText = wrap("test", "<");
-				assert.equal(wrappedText, "<test>");
+				assert.equal(wrap("test", ">"), "<test>");
+				assert.equal(wrap("test", "<"), "<test>");
 			});
 		});
 
 		describe("should wrap with more complex pattern", () => {
 			it("should wrap with <!--", () => {
 				assert.equal(wrap("test", "<!--"), "<!--test--!>");
+				assert.equal(wrap("test", "--!>"), "<!--test--!>");
 			});
 
 			it("should wrap with {{}}", () => {
@@ -67,15 +51,25 @@ describe("wrap", () => {
 				assert.equal(wrap("test", "}}"), "{{test}}");
 			});
 
+			it("should wrap with {{{ }}}", () => {
+				assert.equal(wrap("test", "{{{"), "{{{test}}}");
+				assert.equal(wrap("test", "}}}"), "{{{test}}}");
+			});
+
 			it("should wrap with <%%>", () => {
 				assert.equal(wrap("test", "<%"), "<%test%>");
 				assert.equal(wrap("test", "%>"), "<%test%>");
 			});
+
+			it("should wrap with {%%}", () => {
+				assert.equal(wrap("test", "{%"), "{%test%}");
+				assert.equal(wrap("test", "%}"), "{%test%}");
+			});
 		});
 	});
 
-	describe("should correctly wrap according settings", () => {
-		const wrappedText = wrap("test", ",");
-		assert.equal(wrappedText, ",test,");
+	describe("should correctly wrap with custom pattern", () => {
+		const wrappedText = wrap("test", "<div>");
+		assert.equal(wrappedText, "<div>test<div>");
 	});
 });
--- src/wrap.ts
+++ src/wrap.ts
@@ -1,49 +1,19 @@
-const matchSymbol = {
-	close: {
-		"(": ")",
-		"<": ">",
-		"<%": "%>",
-		"[": "]",
-		"{": "}",
-		"{%": "%}",
-		"{{": "}}",
-		"{{{": "}}}",
-		"«": "»",
-	},
-	open: {
-		"%>": "<%",
-		"%}": "{%",
-		")": "(",
-		">": "<",
-		"]": "[",
-		"}": "{",
-		"}}": "{{",
-		"}}}": "{{{",
-		"»": "«",
-	},
-};
+const template = (str, text) => str.join(text);
 
-const closingSymbol = (ch) => matchSymbol.close[ch] || ch;
-const openingSymbol = (ch) => matchSymbol.open[ch] || ch;
-
-const wrapPattern = (pattern) => {
-	const patternDefined = matchSymbol.open.hasOwnProperty(pattern) ||
-		matchSymbol.close.hasOwnProperty(pattern);
-
-	return [
-		openingSymbol(pattern),
-		patternDefined ?
-			closingSymbol(pattern) :
-			pattern.split("")
-				.reverse()
-				.map(closingSymbol)
-				.join(""),
-	];
-};
-
-const wrap = (text, symbol) => {
-	const [begin, end] = wrapPattern(symbol);
-	return `${begin}${text}${end}`;
+const wrap = (text, pattern) => {
+	switch (pattern) {
+		case "%>": case "<%": return template`<%${text}%>`;
+		case "%}": case "{%": return template`{%${text}%}`;
+		case "(": case ")": return template`(${text})`;
+		case "<": case ">": return template`<${text}>`;
+		case "[": case "]": return template`[${text}]`;
+		case "{": case "}": return template`{${text}}`;
+		case "{{": case "}}": return template`{{${text}}}`;
+		case "{{{": case "}}}": return template`{{{${text}}}}`;
+		case "«": case "»": return template`«${text}»`;
+		case "<!--": case "--!>": return template`<!--${text}--!>`;
+		default: return `${pattern}${text}${pattern}`;
+	}
 };
 
 export default wrap;
